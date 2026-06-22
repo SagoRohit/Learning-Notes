75d570b314a343311fccc864e130b30d8c876609
Sagor Chanda <ch.sagor27@gmail.com>
2026-06-23 02:05:27 +0600

    (proof to work) implementation

--- BlockChain/blockchain.js
+++ BlockChain/blockchain.js
@@ -6,14 +6,23 @@ constructor(index, timestamp, data, previousHash='') {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
     calculateHash(){
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+    mineBlock(difficulty) {
+        while(this.hash.substring(0, difficulty) !== Array(difficulty+1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+        console.log("Block Mined:" + this.hash);
     }
 }
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.difficulty = 5;
     }
     createGenesisBlock() {
         return new Block(0, "10/1/2002", "Genesis Block", "0");
@@ -23,7 +32,7 @@ class Blockchain {
     }
     addBlock(newBlock) {
         newBlock.previousHash = this.getLatestBlock().hash;
-        newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.difficulty)
         this.chain.push(newBlock);
     }
     ischainValid() {
@@ -39,13 +48,16 @@ class Blockchain {
     }
 }
 let bitcoin = new Blockchain();
+console.log("Mining Block 1....")
 bitcoin.addBlock(new Block(1, "2/3/2022", {amount : 4}));
+
+console.log("Mining Block 2....")
 bitcoin.addBlock(new Block(2, "10/3/2022", {amount : 10}));
 
-console.log("Is valid chain: " + bitcoin.ischainValid());
+// console.log("Is valid chain: " + bitcoin.ischainValid());
 
-bitcoin.chain[1].data = {amount : 100};
-bitcoin.chain[1].hash = bitcoin.chain[1].calculateHash();
-console.log("Is valid chain: " + bitcoin.ischainValid());
+// bitcoin.chain[1].data = {amount : 100};
+// bitcoin.chain[1].hash = bitcoin.chain[1].calculateHash();
+// console.log("Is valid chain: " + bitcoin.ischainValid());
 
-console.log(JSON.stringify(bitcoin, null, 4));
+// console.log(JSON.stringify(bitcoin, null, 4));
