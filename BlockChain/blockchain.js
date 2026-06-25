f1652ab0656c5d4685ab17a45395123d43966458
Sagor Chanda <ch.sagor27@gmail.com>
2026-06-25 20:28:25 +0600

    sigining transaction added

--- BlockChain/blockchain.js
+++ BlockChain/blockchain.js
@@ -1,9 +1,31 @@
 const SHA256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
 class Transaction {
     constructor(fromAddress, toAddress, amount) {
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
+    }
+    calculateHash() {
+        return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+    }
+    signTransaction(signingKey) {
+        if(signingKey.getPublic('hex') !== this.fromAddress) // fromAddress is the public key
+            throw new Error('You can not sign transaction for other walleta!');
+        const hashTx = this.calculateHash();
+        const sig = signingKey.sign(hashTx, 'base64'); // we sign the hash using signinkey object
+        this.signature = sig.toDER('hex'); // we save the signature so that we can varify in later that
+    }
+    isvalid() {
+        if(this.fromAddress === null) // miners, so valid transaction
+            return true;
+        if(!this.signature || this.signature.length===0)
+            throw new Error('No signature in this Transaction');
+        const publickey = ec.keyFromPublic(this.fromAddress, 'hex');
+        return publickey.verify(this.calculateHash(), this.signature); // we are varifying if the hash is signed using this signature
+
     }
 }
 class Block {
@@ -24,6 +46,13 @@ constructor(timestamp, transactions, previousHash='') {
         }
         console.log("Block Mined: " + this.hash);
     }
+    hasvalidTransaction() {
+        for(const tx of this.transactions) {
+            if(!tx.isvalid())
+                return false;
+        }
+        return true;
+    }
 }
 class Blockchain {
     constructor() {
@@ -39,6 +68,9 @@ class Blockchain {
         return this.chain[this.chain.length-1];
     }
     minePendingTransactions(miningRewardAddress) {
+        const rewdTx = new Transaction(null, miningRewardAddress, this.miningReward);
+        this.pendingTransactions.push(rewdTx);
+
         let block = new Block(Date.now(), this.pendingTransactions);
         block.mineBlock(this.difficulty);
 
@@ -48,7 +80,12 @@ class Blockchain {
             new Transaction(null, miningRewardAddress, this.miningReward)
         ];
     }
-    createTransaction(transaction) {
+    addTransaction(transaction) {
+        if(!transaction.fromAddress || !transaction.toAddress)
+            throw new Error('No transaction can be made without from and to address');
+        if(!transaction.isvalid())
+            throw new Error('Cannot add invalid transaction..');
+
         this.pendingTransactions.push(transaction);
     }
     getBalanceOfAddress(address) {
@@ -67,6 +104,8 @@ class Blockchain {
         for(let i=1; i<this.chain.length; i++) {
             const current = this.chain[i];
             const previous = this.chain[i-1];
+            if(!current.hasvalidTransaction)
+                return false;
             if(current.hash !== current.calculateHash())
                 return false;
             if(previous.hash !== current.previousHash)
@@ -74,31 +113,8 @@ class Blockchain {
         }
         return true;
     }
+
 }
-let bitcoin = new Blockchain();
 
-bitcoin.createTransaction(new Transaction("address1", "address2", 100));
-bitcoin.createTransaction(new Transaction("address2", "address1", 50));
-
-console.log('\nStarting the miner...');
-bitcoin.minePendingTransactions("miner1");
-console.log("\nBalance of miner ", bitcoin.getBalanceOfAddress("miner1"));
-
-console.log('\nStarting Miner again...');
-bitcoin.minePendingTransactions("miner1");
-console.log('\nBalance of miner ', bitcoin.getBalanceOfAddress('miner1'));
-
-
-// console.log("Mining Block 1....")
-// bitcoin.addBlock(new Block(1, "2/3/2022", {amount : 4}));
-
-// console.log("Mining Block 2....")
-// bitcoin.addBlock(new Block(2, "10/3/2022", {amount : 10}));
-
-// console.log("Is valid chain: " + bitcoin.ischainValid());
-
-// bitcoin.chain[1].data = {amount : 100};
-// bitcoin.chain[1].hash = bitcoin.chain[1].calculateHash();
-// console.log("Is valid chain: " + bitcoin.ischainValid());
-
-// console.log(JSON.stringify(bitcoin, null, 4));
+module.exports.Blockchain = Blockchain;
+module.exports.Transaction = Transaction;
